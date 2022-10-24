966cff898612ed0c07f2b29b213df3097bf7e2d1
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-10-24 17:12:08 -0500

    add interface file

---
+++ internal/video/interfaces.go
@@ -0,0 +1,23 @@
+package video
+
+import (
+	"context"
+
+	"github.com/marcos-nsantos/aluraflix-api/internal/entity"
+)
+
+type Writer interface {
+	Create(ctx context.Context, video *entity.Video) error
+	Update(ctx context.Context, video *entity.Video) error
+	Delete(ctx context.Context, id uint64) error
+}
+
+type Reader interface {
+	FindAll(ctx context.Context) ([]*entity.Video, error)
+	FindByID(ctx context.Context, id uint64) (*entity.Video, error)
+}
+
+type Repor interface {
+	Writer
+	Reader
+}
