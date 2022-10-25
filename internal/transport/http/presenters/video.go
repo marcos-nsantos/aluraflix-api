cf139cc02687f6ed15c4f8108a0178f7fbdf4a66
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-10-25 07:58:45 -0500

    add function to return a video entity representation

---
+++ internal/transport/http/presenters/video.go
@@ -0,0 +1,23 @@
+package presenters
+
+import "github.com/marcos-nsantos/aluraflix-api/internal/entity"
+
+type Video struct {
+	ID          uint64 `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description,omitempty"`
+	URL         string `json:"url"`
+	CreatedAt   string `json:"created_at,omitempty"`
+	UpdatedAt   string `json:"updated_at,omitempty"`
+}
+
+func VideoResponse(video *entity.Video) *Video {
+	return &Video{
+		ID:          video.ID,
+		Title:       video.Title,
+		Description: video.Description,
+		URL:         video.URL,
+		CreatedAt:   video.CreatedAt.Format("2006-01-02 15:04:05"),
+		UpdatedAt:   video.UpdatedAt.Format("2006-01-02 15:04:05"),
+	}
+}
